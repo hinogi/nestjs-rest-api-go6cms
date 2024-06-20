c2b4b033c6317a229112cc57dcc4d135c0b4a121
hinogi <stefan.schneider@gmx.net>
2024-06-20 08:53:30 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setDescription('유저 정보를 관리하기 위한 샘플 REST API 입니다.')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
